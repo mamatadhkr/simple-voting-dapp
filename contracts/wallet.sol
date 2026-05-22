feed0e39e3df210cf90fcb434db68048ded53944
mamatadhkr <mamatadhkr123@gmail.com>
2026-05-22 20:56:43 +0545

    Update wallet.sol

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -1,1 +1,15 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SimpleWallet {
+address public owner;
+constructor() {
+        owner = msg.sender;}
+// Deposit ETH into contract
+    function deposit() public payable {
+}// Check contract balance
+    function getBalance() public view returns(uint) {
+        return address(this).balance; }
+// Withdraw ETH
+    function withdraw() public {
+  require(msg.sender == owner, "Not owner");
+        payable(owner).transfer(address(this).balance);  }}
